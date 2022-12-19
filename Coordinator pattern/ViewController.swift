303fd955cecab76bf59abbd05836b254ef5a8b3b
Artem Galay <iostema@mail.ru>
2022-12-19 18:54:19 +0300

    Initial Commit

---
+++ Coordinator pattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coordinator pattern
+//
+//  Created by Артем Галай on 19.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
